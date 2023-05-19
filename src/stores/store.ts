37af324e462059b2be05948c84248b5a63b75a3f
mucahitkurtlar <mucahit@tutanota.com>
2023-05-19 21:36:19 +0300

    Extract token from cookie properly
    - Extract JWT token from cookies with more proper way

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -4,6 +4,8 @@ export const axiosInstance = axios.create({
     baseURL: 'http://localhost:2323/api',
     timeout: 1000,
     headers: {
-        Authorization: 'Bearer ' + document.cookie.split('=')[1]
+        Authorization: 'Bearer ' + (() => {
+                return document.cookie.split('; ').find(row => row.startsWith('token'))?.split('=')[1] || ''
+        })()
     }
 })
