249a5dcb894c002d23efcde5394f545cf0521129
mucahitkurtlar <mucahit@tutanota.com>
2023-05-19 21:38:03 +0300

    Show time in hh:mm format
    - Show datetime in message boxes in hh:mm format

--- src/components/msgbox/index.tsx
+++ src/components/msgbox/index.tsx
@@ -6,7 +6,7 @@ function MsgBox({ message }: { message: Message }) {
     <div className="msg-box">
       <div className="msg-box__author">{message.author}</div>
       <div className="msg-box__content">{message.content}</div>
-      <div className="msg-box__time">{message.time}</div>
+      <div className="msg-box__time">{message.time.slice(11, 16)}</div>
     </div>
   )
 }
