44677db59a7a23bfdd7afb6ec36a0a10d8e42c1a
mucahitkurtlar <mucahit@tutanota.com>
2023-05-24 15:54:24 +0300

    Update ESLint configuration
    - Add node environment
    - Add new rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -10,5 +10,8 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react-hooks/exhaustive-deps': 'off',
+    'no-multiple-empty-lines': ['warn', { max: 1, maxEOF: 1 }],
+    quotes: ['warn', 'single', { avoidEscape: true }],
   },
 }
