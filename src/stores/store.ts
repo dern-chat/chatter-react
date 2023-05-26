bf46925bb5d3579973aa92a7952e0f0b543db6fa
mucahitkurtlar <mucahit@tutanota.com>
2023-05-26 19:55:20 +0300

    Change timeout
    - Change axios timeout 1000 ms to 10000 ms

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
   baseURL: 'http://localhost:2323/api',
-  timeout: 1000,
+  timeout: 10000,
   headers: {
     Authorization: 'Bearer ' + (() => {
       return document.cookie.split('; ').find(row => row.startsWith('token'))?.split('=')[1] || ''
